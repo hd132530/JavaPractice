0590bf615c38a9cddc46166b91958d5a0dab28e3
승진 <@No_Doh_Nut>
2015-08-12 15:32:00 +0900

    .

---
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *        |_FigureTest
+ * 
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ * 
+ * @author : Seungjin Jeong
+ * @version : 1.0
+ */
+public class FigureTest extends Figure {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/Tri.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *        |_Tri
+ * 
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ * 
+ * @author : Seungjin Jeong
+ * @version : 1.0
+ */
+public class Tri extends Figure {
+
+}
