d53489ddde5c4ac9eedfc76de9506df5b85d60fd
승진 <@No_Doh_Nut>
2015-08-31 09:28:02 +0900

    .

--- JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -21,19 +21,37 @@ public class SmartPhone{
 	private String model;
 	private String company;
 	private int price;
+	
+	public SmartPhone(){
+	}
 
 	public SmartPhone(String model, String company, int price) {
+		super();
 		this.model = model;
 		this.company = company;
 		this.price = price;
 	}
 	
-	protected void turnOn(){
-		
-		
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
 	}
-	protected void turnOff(){
-		
-	}	
+	
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public void pay() {}
+	
+	public void useSpecialFunction() {
+		if(this instanceof IPhone) {
+			((IPhone) this).useAirDrop();
+		} else if(this instanceof Galaxy) {
+			((Galaxy) this).userWirelessCharging();
+		}
+	}
+
+	public String toString() {
+		return "모델명 : " + model + ", 제조사 : " + company	+ ", 가격 : " + String.format("%,d", price) + "원";
+	}
 	
 }
--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -20,5 +20,16 @@ public class SmartPhoneTest extends SmartPhone {
 
 	public static void main(String[] args) {
 		
+		SmartPhone[] pl = {new IPhone("iPhone 6", "애플", 700000), new Galaxy("갤럭시 S6", "삼성", 650000)};
+		
+		for(SmartPhone phone : pl) {
+			System.out.println(phone.toString());
+			
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+		
+		}
 	}
 }
