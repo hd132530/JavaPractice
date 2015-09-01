4d5ec13dee22f178edac069b1c1e6d757794e301
승진 <@No_Doh_Nut>
2015-09-01 10:17:52 +0900

    .

---
+++ JavaPractice/src/org/dimigo/abstractclass/Galaxy.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *        |_Circle
+ * 
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ * 
+ * @author : Seungjin Jeong
+ * @version : 1.0
+ */
+public class Galaxy extends SmartPhone {
+	
+	public Galaxy(){
+	}
+	
+	public Galaxy(String model, String company, int price) {
+		super(model, company, price);
+	}
+	
+	public void pay() {
+		System.out.println("삼성 페이로 결제합니다.");
+	}
+	
+	public void userWirelessCharging() {
+		System.out.println("무선 충전 기능을 이용합니다.");
+	}
+	
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *        |_Circle
+ * 
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 8. 12.
+ * </pre>
+ * 
+ * @author : Seungjin Jeong
+ * @version : 1.0
+ */
+public abstract class SmartPhoneTest extends SmartPhone {
+
+	public static void main(String[] args) {
+		
+		SmartPhone[] pl = {new IPhone("iPhone 6", "애플", 700000), new Galaxy("갤럭시 S6", "삼성", 650000)};
+		
+		for(SmartPhone phone : pl) {
+			System.out.println(phone.toString());
+			
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+		
+		}
+	}
+}
